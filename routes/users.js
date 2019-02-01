d1499940fa1755ef643b2623992d189f22096ffb
fitsum belayneh <fitsu_alt@yahoo.com>
2019-02-01 12:03:01 -0500

    destructuring and validation

--- routes/users.js
+++ routes/users.js
@@ -9,8 +9,35 @@ router.get("/Register", (req, res) => res.render("Register"));
 
 // Register Handle
 router.post("/Register", (req, res) => {
-  console.log(req.body);
-  res.send("hello");
+  const { name, email, password, password2 } = req.body;
+  let errors = [];
+
+  // Check required fields
+  if (!name || !email || !password || !password2) {
+    errors.push({ msg: "plese fill in all fields" });
+  }
+
+  // Check password match
+  if (password !== password2) {
+    errors.push({ msg: "passwords do not match" });
+  }
+
+  // Check pass length
+  if (password.length < 6) {
+    errors.push({ msg: "password should be at least 6 characters" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", {
+      errors,
+      name,
+      email,
+      password,
+      password2
+    });
+  } else {
+    res.send("pass");
+  }
 });
 
 module.exports = router;
