550e24412ec8a5e171875eb547e6ec052d537e42
fitsum belayneh <fitsu_alt@yahoo.com>
2019-02-01 11:35:29 -0500

    building a schema

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,10 @@ router.get("/Login", (req, res) => res.render("Login"));
 // Register Page
 router.get("/Register", (req, res) => res.render("Register"));
 
+// Register Handle
+router.post("/Register", (req, res) => {
+  console.log(req.body);
+  res.send("hello");
+});
+
 module.exports = router;
